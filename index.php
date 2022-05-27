881ebb7a0d1a1d9874d1e70e86c11e74bf7bf4d0
indahqurrothul <56025305+indahqurrothul@users.noreply.github.com>
2022-05-28 00:06:13 +0700

    commit3

--- index.php
+++ index.php
@@ -28,17 +28,7 @@
 		                  <li data-target="#carousel-example-generic" data-slide-to="1" class=""></li>
 		                  <li data-target="#carousel-example-generic" data-slide-to="2" class=""></li>
 		                </ol>
-		                <div class="carousel-inner">
-		                  <div class="item active">
-		                    <img src="images/banner1.png" alt="First slide">
-		                  </div>
-		                  <div class="item">
-		                    <img src="images/banner2.png" alt="Second slide">
-		                  </div>
-		                  <div class="item">
-		                    <img src="images/banner3.png" alt="Third slide">
-		                  </div>
-		                </div>
+		            
 		                <a class="left carousel-control" href="#carousel-example-generic" data-slide="prev">
 		                  <span class="fa fa-angle-left"></span>
 		                </a>
